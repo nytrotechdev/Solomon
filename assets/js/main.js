7cea800299a226d9a2bdda5992d7142205d674bb
nytrotechdev <nytrotech.dev@gmail.com>
2022-05-12 18:28:36 +0500

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+// Transition Animation
+
+TweenMax.from('.main_home', 2,{
+    delay: .2,
+    width: 0,
+    opacity: 0,
+    ease: Power2.easeInOut
+})
+
+TweenMax.from('.main2', 2,{
+    delay: .1,
+    width: 0,
+    opacity: 0,
+    ease: Power2.easeInOut
+})
