a3c0089567f4c53bb86a929d2aa7c6b354753d3a
nytrotechdev <nytrotech.dev@gmail.com>
2022-05-26 12:00:32 +0500

    changes done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,22 @@
-// Transition Animation
+// // Transition Animation
 
-TweenMax.from('.main_home', 2,{
-    delay: .2,
-    width: 0,
-    opacity: 0,
-    ease: Power2.easeInOut
-})
+// TweenMax.from('.main_home', 2,{
+//     delay: .2,
+//     width: 0,
+//     opacity: 0,
+//     ease: Power2.easeInOut
+// })
 
-TweenMax.from('.home_section', 2,{
-    delay: .1,
-    width: 0,
-    opacity: 0,
-    ease: Power2.easeInOut
-})
+// TweenMax.from('.home_section', 2,{
+//     delay: .1,
+//     width: 0,
+//     opacity: 0,
+//     ease: Power2.easeInOut
+// })
 
-TweenMax.from('.project_section', 2,{
-    delay: .1,
-    width: 0,
-    opacity: 0,
-    ease: Power2.easeInOut
-})
+// TweenMax.from('.project_section', 2,{
+//     delay: .1,
+//     width: 0,
+//     opacity: 0,
+//     ease: Power2.easeInOut
+// })
